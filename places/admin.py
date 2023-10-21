b540a301b8bbc01bf8c65fe4aecadfc739cd9194
zatomis <zatomis@list.ru>
2023-10-21 11:33:11 +0300

    Add Places model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
 # Register your models here.
+from places.models import Places
+
+admin.site.register(Places)
--- places/models.py
+++ places/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Places(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название места', null=True)
+    description_short = models.CharField(max_length=400, verbose_name='Описание места - кратко')
+    description_long = models.TextField(verbose_name='Подробное описание места')
+    lon = models.FloatField(blank=False, verbose_name='долгота')
+    lat = models.FloatField(blank=False, verbose_name='широта')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
