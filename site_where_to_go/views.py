9b55cb1a31d19577bc7c2e8b0d41a28f640ddf33
zatomis <zatomis@list.ru>
2023-10-21 10:06:13 +0300

    Create places app

--- site_where_to_go/views.py
+++ site_where_to_go/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 def show_main(request):
-    template = loader.get_template('start.html')
+    template = loader.get_template('index.html')
     context = {}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
