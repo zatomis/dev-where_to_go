b395118d5a887545545d55df7a6ade8b8d22bc6d
zatomis <zatomis@list.ru>
2023-11-26 11:50:58 +0300

    Отображение превью картинок

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from places.models import Place, Pictures
 
 admin.site.register(Pictures)
@@ -7,6 +9,10 @@ admin.site.register(Pictures)
 class ImageInline(admin.TabularInline):
     model = Pictures
     extra = 1
+    readonly_fields = ["pic_preview"]
+
+    def pic_preview(self, obj):
+        return format_html('<img src="{}" style="max-height: 150px;"/>', mark_safe(obj.picture.url))
 
 
 @admin.register(Place)
